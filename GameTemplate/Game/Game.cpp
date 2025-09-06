c3fd3ab3a44f1733a229712a1fb7c6d1df7b3308
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2025-09-06 17:01:24 +0900

    ステージ2モデルの追加

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -1,6 +1,4 @@
 #include "stdafx.h"
-#include "ChaseEnemy.h"
-#include "Door.h"	
 #include "Game.h"
 #include "GameCamera.h"
 #include "Player.h"
@@ -12,28 +10,6 @@ bool Game::Start()
 	m_stage1 = NewGO<Stage1>(0, "stage1");
 	m_gameCamera = NewGO<GameCamera>(0, "gamecamera");
 
-	//EnemyNewGO(); // í«è]ìGÇÃê∂ê¨ÅB
-	m_chaseEnemy = FindGO<ChaseEnemy>("chaseEnemy");
-	
-//	PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
+	PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
 	return true;
-}
-
-
-/// <summary>
-///	í«è]ìGÇÃê∂ê¨ÅB
-/// </summary>
-void Game::EnemyNewGO()
-{
-	m_chaseEnemy = NewGO<ChaseEnemy>(0, "chaseEnemy");
-	m_chaseEnemy->SetPosition(Vector3{ 200.0f, 0.0f, -0.0f });// í«è]ìGÇÃç¿ïWê›íËÅB
-	m_chaseEnemy->m_EnemyPos = m_chaseEnemy->m_ChaseEnemyPos;
-	m_modelRender.SetPosition(m_pos);
-
-}
-
-void Game::DoorNewGO()
-{
-	m_door = NewGO<Door>(0,"door");
-// 	m_door-
 }
--- GameTemplate/Game/Stage1.cpp
+++ GameTemplate/Game/Stage1.cpp
@@ -3,7 +3,7 @@
 
 namespace
 {
-	const Vector3 SCALE = Vector3(1.0f, 1.0f, 1.0f);
+	const Vector3 SCALE(1.0f, 1.0f, 1.0f);
 	const Vector3 COLLISION_HEIGHT(0.0f, 50.0f, 0.0f);//コリジョンの高さ
 	const Vector3 COLLISION_SIZE(100.0f, 100.0f, 100.0f);//コリジョンの大きさ
 }
@@ -15,30 +15,33 @@ Stage1::~Stage1()
 
 bool Stage1::Start()
 {
-	m_StageRender.Init("Assets/stage&material/stage1.tkm");
+	m_StageRender.Init("Assets/stage&material/Stage1/Stage1.tkm");
 	m_physicsStaticObject.CreateFromModel(m_StageRender.GetModel(), m_StageRender.GetModel().GetWorldMatrix());
-    
+	m_StageRender.SetScale(SCALE);
+
 
 	// コリジョン。
 	m_collisionObject = NewGO<CollisionObject>(0, "collisionobject");
 
 	//コリジョンを動く床に設置
 	m_collisionObject->CreateBox
-	(
-		m_position + COLLISION_HEIGHT,
+	(   m_position + COLLISION_HEIGHT,
 		Quaternion::Identity,
-		COLLISION_SIZE
-	);
+		COLLISION_SIZE );
+
+	// 座標設定。
+	m_StageRender.SetPosition(m_position);
+	m_initPosition = m_position;
 
 	// コリジョンを破棄。
 	m_collisionObject->SetIsEnableAutoDelete(false);
 
+	m_StageRender.Update();
 	return true;
 }
 
 void Stage1::Update()
 {
-	m_StageRender.SetScale(SCALE);
 	m_StageRender.Update();
 
 	// 当たり判定。

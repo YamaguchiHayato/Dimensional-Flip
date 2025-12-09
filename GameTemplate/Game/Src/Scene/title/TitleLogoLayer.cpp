9d9d2944883b7d3b6f32be6c644451f329c6e556
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2025-12-09 20:11:05 +0900

    タイトル画面の改修

--- GameTemplate/Game/Src/Scene/title/TitleLogoLayer.cpp
+++ GameTemplate/Game/Src/Scene/title/TitleLogoLayer.cpp
@@ -4,41 +4,40 @@
 namespace
 {
     // ボタン画像のステータス。
-    const float BUTTON_WIDTH = 500.0f;
-    const float BUTTON_HEIGHT = 500.0f;
-    const Vector3 BUTTON_SCALE(3.0f, 3.0f, 3.0f);
+    const float BUTTON_WIDTH = 512.0f;
+    const float BUTTON_HEIGHT = 512.0f;
+    const Vector3 BUTTON_SCALE(Vector3::One);
 
     // タイトルロゴ画像のステータス。
-    const float LOGO_WIDTH = 600.0f;
-    const float LOGO_HEIGHT = 600.0f;
+    const float LOGO_WIDTH = 687.0f;
+    const float LOGO_HEIGHT = 141.0f;
 
 }
 
 bool TitleLogoLayer::Start()
 {
     // タイトルロゴ。
-//    std::string layerName = Init("TitleText");
-    std::string layerName = Init("logo");
-
+    std::string layerName = Init("title");
     titleLayerRender_.Init(layerName.c_str(), LOGO_WIDTH, LOGO_HEIGHT);
 
     // プレスボタン。
-    std::string Abutton = Init("button");
+    std::string Abutton = Init("Button");
     pressButtonRender_.Init(Abutton.c_str(), BUTTON_WIDTH, BUTTON_HEIGHT);
 
     return true;
 }
 
+
 void TitleLogoLayer::Update()
 {
     // タイトルロゴ。
-    titleLayerRender_.SetPosition({ 0.0f, 300.0f, 0.0f });
-    titleLayerRender_.SetScale(Vector3(3.0f, 3.0f, 3.0f));
+    titleLayerRender_.SetPosition({ 0.0f, 100.0f, 0.0f });
+    titleLayerRender_.SetScale(Vector3(2.0f, 2.0f, 2.0f));
     titleLayerRender_.Update();
 
     // プレスボタン。
     pressButtonRender_.SetScale(BUTTON_SCALE);
-    pressButtonRender_.SetPosition(Vector3(0.0f, -400.0f, 0.0f));
+    pressButtonRender_.SetPosition(Vector3(0.0f, -100.0f, 0.0f));
     pressButtonRender_.Update();
 }
 
@@ -49,5 +48,5 @@ void TitleLogoLayer::Render(RenderContext& rc)
     titleLayerRender_.Draw(rc);
 
     // プレスボタン描画。
-    pressButtonRender_.Draw(rc);
+//    pressButtonRender_.Draw(rc);
 }

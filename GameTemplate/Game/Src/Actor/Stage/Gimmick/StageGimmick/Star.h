613911a682e2be4051e195b58229de0178fa2595
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2026-02-05 11:32:52 +0900

    <演出> スターを取得する演出のベースの作成

--- GameTemplate/Game/Src/Actor/Stage/Gimmick/StageGimmick/Star.h
+++ GameTemplate/Game/Src/Actor/Stage/Gimmick/StageGimmick/Star.h
@@ -13,18 +13,44 @@ class CutInView;
 class Star : public IGameObject
 {
 public:
-	Star() {};
-	virtual ~Star() {};
+	Star() = default;
+	virtual ~Star() = default;
+
+
+public:
 	bool Start()override;
 	void Update()override;
 	void Render(RenderContext& rc)override;
+
+
+private:
+    // 回転処理。
 	void Rotation();
 
+    // 移動処理全体を更新。
+    void UpdateMovement(float deltaTime);
+
+    // 演出終了時の処理。
+    void OnEffectFinished();
+
+
+// セッター。
+public:
+    // 座標をセット。
 	inline void SetStarPosition(const Vector3& starPosition)
 	{
 		m_starPosition = starPosition;
 	}
-	const Vector3& GetStarPosition()
+
+    // 演出をする時間をセット。
+    inline void SetDisplayDuration(float duration)
+    {
+        displayDuration_ = duration;
+    }
+
+// ゲッター。
+public:
+	inline const Vector3& GetStarPosition()
 	{
 		return m_starPosition;
 	}
@@ -34,11 +60,19 @@ private:
 	Player* m_player = nullptr;
     CutInView* pCutInView_ = nullptr;
 
+
 private:
 	ModelRender m_starRender;
-	Vector3 m_starPosition;
-    Quaternion m_starRotation;
-    bool m_isGoal = false;
 
+	Vector3 m_starPosition = Vector3::Zero;
+
+    Quaternion m_starRotation = Quaternion::Identity;
+    Quaternion currentRotation_ = Quaternion::Identity;
+
+    bool m_isGoal = false;
+    bool isCollected_ = false;
+
+    float disPlayTimer_ = 0.0f;
+    float displayDuration_ = 0.0f;
 };
 

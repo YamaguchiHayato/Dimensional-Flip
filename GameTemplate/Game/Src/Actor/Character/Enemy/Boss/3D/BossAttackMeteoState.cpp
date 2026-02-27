d8b9352d780be17219d6e50ff24277f0d412059f
YamaguchiHayato <167502303+YamaguchiHayato@users.noreply.github.com>
2026-02-27 18:38:52 +0900

    <ボス戦> MeteoStateの改修 (#129)

--- GameTemplate/Game/Src/Actor/Character/Enemy/Boss/3D/BossAttackMeteoState.cpp
+++ GameTemplate/Game/Src/Actor/Character/Enemy/Boss/3D/BossAttackMeteoState.cpp
@@ -2,6 +2,8 @@
 #include "BossAttackMeteoState.h"
 
 #include "Src/Actor/Character/Enemy/Boss/Boss.h"
+#include "Src/Core/BossUIManager.h"
+#include "Src/Core/SoundManager.h"
 #include "Src/Actor/Stage/Gimmick/BossGimmick/Meteo.h"
 
 
@@ -32,6 +34,9 @@ namespace app
 
             // 攻撃タイプをセット。
             pBoss_->SetAttackType(app::enemyStatus::AttackType::Meteor);
+
+            // 攻撃UIをセット。
+            app::nsUI::BossUIManager::GetInstance().OnNotifyAttack(app::nsUI::BossAttackKind::Meteor);
         }
 
 
@@ -54,29 +59,47 @@ namespace app
             // 次のインターバルをランダムに設定。
             auto interval = 2.0f + (static_cast<float>(rand() % 16) / 10.0f);
             pBoss_->SettNextInterval(interval);
+
+            // UIを非表示に。
+            app::nsUI::BossUIManager::GetInstance().OnNotifyAttack(app::nsUI::BossAttackKind::Meteor);
+
+            // ステートを抜ける際はSEを停止する。
+            app::core::SoundManager::GetInstance()->StopSE(GameSoundList_SE_SpawnBlock);
         }
+
 
         bool BossAttackMeteoState::IsFinished() const
         {
-
             return timer_ >= ATTACK_DURATION;
         }
 
 
         void BossAttackMeteoState::CreateMeteoAttack()
         {
+            // 隕石を落下させるSEを再生。
+            app::core::SoundManager::GetInstance()->PlaySE(GameSoundList_SE_SpawnBlock, 2.0f);
+
             for (int i = 0; i < SPAWN_NUM; i++)
             {
                 // 座標はBossに計算してもらう
-                Vector3 spawnPos = pBoss_->GetRandomAttackPos();
+                Vector3 targetPos = pBoss_->GetRandomAttackPos(); 
+                Vector3 spawnPos = targetPos;
+                spawnPos.y += 200.0f;
 
                 // 隕石生成
                 auto speed = 2.5f + static_cast<float>(rand() % 5);
                 auto delay = 0.2f + (static_cast<float>(rand() % 15) / 10.0f);
 
+                // 隕石クラスの生成。
                 auto meteo = NewGO<app::gimmick::Meteo>(0);
                 meteo->SetName("meteo");
-                meteo->SetTargetPos(spawnPos);
+
+                // 落下し始める座標のセット。
+                meteo->SetPosition(spawnPos);
+                // 落下し終わる座標をセット。
+                meteo->SetTargetPos(targetPos);
+
+                // 落下速度と警告時間を設定。
                 meteo->SetParams(speed, delay);
             }
         }
--- GameTemplate/Game/Src/Actor/Stage/Gimmick/BossGimmick/Meteo.cpp
+++ GameTemplate/Game/Src/Actor/Stage/Gimmick/BossGimmick/Meteo.cpp
@@ -1,9 +1,14 @@
 #include "stdafx.h"
 #include "Src/Actor/Stage/Gimmick/BossGimmick/Meteo.h"
+#include "Src/Actor/Character/Player/Player.h"
 
 namespace
 {
+    // 警告時間。
     const auto WARNING_TIME = 2.0f;
+
+    // 当たり判定の半径。
+    const auto HIT_RADIUS = 15.0f;
 }
 
 
@@ -13,17 +18,21 @@ namespace app
     {
         bool Meteo::Start()
         {
+            // モデル名をセット。
             const std::string meteoPath = InitGimmick("StageEX/FallingRocks");
             render_.Init(meteoPath.c_str());
 
-
+            // Playerを探索。
+            pPlayer_ = FindGO<Player>("player");
+            hasHit = false;
 
             // 初期位置をセット。
             render_.SetPosition(currentPos_);
 
             // マーカーを生成する。
             pMarker_ = NewGO<app::production::AttackMarker>(0);
-            pMarker_->SetTransform(targetPos_, Vector3(0.5f, 0.5f, 0.5f));
+            pMarker_->SetTransform(targetPos_, Vector3::One * 0.25);
+
             // 予兆時間をセットする。
             pMarker_->SetDuration(warningTime_);
 
@@ -35,6 +44,7 @@ namespace app
         {
             // 状態を更新する。
             UpdateState();
+
             // スケールをセットする。
             render_.SetScale(Vector3(0.05f, 0.05f, 0.05f));
             // 座標を更新する。
@@ -74,8 +84,15 @@ namespace app
                 // 落下処理。
                 currentPos_.y -= fallSpeed_;
 
+                // 当たっていなければ当たり判定を行う。
+                if (!hasHit)
+                    if (CheckCollision())
+                        hasHit = true;
+
+                // 削除までの時間を加算する。
                 lifeTime_ += g_gameTime->GetFrameDeltaTime();
 
+                // 一定時間経過するか、地面に到達したら削除する。
                 if (lifeTime_ > 10.0f || currentPos_.y < -300.0f)
                 {
                     if (pMarker_)
@@ -88,14 +105,36 @@ namespace app
                 }
                 break;
             }
-
-
-            default:
-                break;
             }
 
         }
-         
+
+
+        bool Meteo::CheckCollision()
+        {
+            // 早期リターン。
+            if (!pPlayer_)
+                return false;
+
+            // プレイヤーの座標を取得する。
+            Vector3 playerPos = pPlayer_->GetPlayerPos();
+            playerPos.y += 20.0f;
+
+            // 距離判定。
+            Vector3 diff = currentPos_ - playerPos;
+            if (diff.LengthSq() <= HIT_RADIUS * HIT_RADIUS)
+            {
+                // ダメージ処理。
+                pPlayer_->OnDamage(1);
+                hasHit = true;
+
+                DeleteGO(this);
+                return true;
+            }
+
+            return false;
+        }
+
 
         void Meteo::Render(RenderContext& rc)
         {
